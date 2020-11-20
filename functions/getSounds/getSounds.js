12d2d06987116efe0acc9138bb079e3026fd6806
I852708 <daniel.bertolozi@sap.com>
2020-11-20 16:47:09 -0300

    c

--- functions/getSounds/getSounds.js
+++ functions/getSounds/getSounds.js
@@ -1,20 +1,20 @@
 const fetch = require('node-fetch')
 const fs = require("fs");
 
-const handler = async function () {
+const handler = async function (event, context, callback) {
   try {
     //const response = await fetch('https://icanhazdadjoke.com', {
 //      headers: { Accept: 'application/json' },
  //   })
 	const contents = fs.readFileSync(require.resolve("./SAPUI5.mp3"));
 
-    return {
+    callback({
       statusCode: 200,
 	  headers: {
 		"Access-Control-Allow-Origin": "*"
 	  },
       body: contents,
-    }
+    });
   } catch (error) {
     // output to netlify function log
     console.log(error)
